2d20554a23f7ff229b4d10d80dca11711a2283b5
Nudelman Mauricio <nudelman.mauricio@gmail.com>
2014-04-29 09:16:49 -0300

    wf

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 import java.util.logging.Level;
@@ -17,9 +12,6 @@ import javax.persistence.Persistence;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         EntityManagerFactory emf = null;
         EntityManager em = null;
