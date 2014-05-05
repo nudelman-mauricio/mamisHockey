253130d1c393f8cf72fd03bf3bff915a6dffb427
L00kas <lucas.deicastelli@gmail.com>
2014-05-05 16:55:27 -0300

    Otras Mejoras

--- src/Interfaces/InterfaceGestionSocias.java
+++ src/Interfaces/InterfaceGestionSocias.java
@@ -45,7 +45,7 @@ public class InterfaceGestionSocias extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Gestión de Socias");
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
