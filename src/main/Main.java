bbed5afd69f1a8ce877164302c3cb16b6957cb7a
Leanwit <leanwitzke@gmail.com>
2014-05-06 08:31:26 -0300

    Skin a la  ventana principal

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
 import Interfaces.MenuPrincipalInterface;
+import com.l2fprod.gui.plaf.skin.Skin;
+import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  *
@@ -47,6 +51,19 @@ public class Main {
         if (emf != null) {
             emf.close();
         } */
+        
+        //Aplicar skin a la aplicacion. 
+        try {
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            Skin theSkinToUse = SkinLookAndFeel.loadThemePack("skins/underlingthemepack.zip");
+            SkinLookAndFeel.setSkin(theSkinToUse);
+            UIManager.setLookAndFeel(new SkinLookAndFeel());
+            
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        //Creacion de la ventana
         MenuPrincipalInterface unaVentana = new MenuPrincipalInterface ();
         SwingUtilities.updateComponentTreeUI ( unaVentana ) ;        
         unaVentana.setVisible(true);
