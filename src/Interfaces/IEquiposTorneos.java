5ef78451a320817228f5ca9af44049d176012364
Leanwit <leanwitzke@gmail.com>
2014-07-23 22:40:52 -0300

    Equipos Torneo

--- src/Interfaces/IEquiposTorneos.java
+++ src/Interfaces/IEquiposTorneos.java
@@ -37,11 +37,12 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
 
     public IEquiposTorneos(ControladoraGlobal unaControladoraGlobal, JInternalFrame unJInternalFrame, Torneo unTorneo) {
         initComponents();
+        IMenuPrincipalInterface.centrar(this);
         this.unJInternalFrame = unJInternalFrame;
         this.unaControladoraGlobal = unaControladoraGlobal;
         this.unTorneo = unTorneo;
         this.modeloTablaEquipoInscripto = (DefaultTableModel) jTableEquiposInscriptos.getModel();
-        this.modeloTablaEquipoDisponible = (DefaultTableModel) jTableEquiposInscriptos.getModel();
+        this.modeloTablaEquipoDisponible = (DefaultTableModel) jTableEquiposDisponibles.getModel();
         this.setTitle("Torneo: " + unTorneo.getNombre());
         SeInicio();
         cargarTabla();
@@ -50,11 +51,21 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
     public void SeInicio() {
         //Icono de la ventana
         setFrameIcon(new ImageIcon(getClass().getResource("../Iconos Nuevos/Equipoo.png")));
-
         camposActivo(false);
 
     }
 
+     private void limpiarTablaDisponible(DefaultTableModel modeloTablaEquipoDisponible) {
+        try {
+            int filas = modeloTablaEquipoDisponible.getRowCount();
+            for (int i = 0; i < filas; i++) {
+                modeloTablaEquipoDisponible.removeRow(0);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al limpiar la tabla.");
+        }
+     }
+    
     private void limpiarTabla(DefaultTableModel modeloTablaEquipoInscripto, DefaultTableModel modeloTablaEquipoDisponible) {
         try {
             int filas = modeloTablaEquipoInscripto.getRowCount();
@@ -78,7 +89,6 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
     }
 
     public void camposActivo(boolean Editable) {
-
         jButtonCancelar.setEnabled(Editable);
     }
 
@@ -106,6 +116,8 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
         }
 
     }
+    
+   
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -128,12 +140,25 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
         jTableEquiposInscriptos = new javax.swing.JTable();
         jScrollPane4 = new javax.swing.JScrollPane();
         jTableEquiposDisponibles = new javax.swing.JTable();
-        jTextFieldBuscar = new javax.swing.JTextField();
-        jRadioButtonDni3 = new javax.swing.JRadioButton();
-        jRadioButtonApellido3 = new javax.swing.JRadioButton();
-        jRadioButtonDni4 = new javax.swing.JRadioButton();
 
         setClosable(true);
+        addInternalFrameListener(new javax.swing.event.InternalFrameListener() {
+            public void internalFrameActivated(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameClosed(javax.swing.event.InternalFrameEvent evt) {
+                formInternalFrameClosed(evt);
+            }
+            public void internalFrameClosing(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameDeactivated(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameDeiconified(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameIconified(javax.swing.event.InternalFrameEvent evt) {
+            }
+            public void internalFrameOpened(javax.swing.event.InternalFrameEvent evt) {
+            }
+        });
 
         jPanelBotones.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -198,6 +223,11 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
 
         jButtonAgregar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos Nuevos/next.png"))); // NOI18N
         jButtonAgregar.setEnabled(false);
+        jButtonAgregar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonAgregarMouseClicked(evt);
+            }
+        });
         jButtonAgregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonAgregarActionPerformed(evt);
@@ -212,28 +242,29 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
             }
         });
 
+        jScrollPane2.setBorder(javax.swing.BorderFactory.createTitledBorder("Equipos Inscriptos"));
+
         jTableEquiposInscriptos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Nombre", "Club", "Categoria", "id"
+                "id", "Nombre", "Club", "Categoria"
             }
         ));
         jTableEquiposInscriptos.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 jTableEquiposInscriptosFocusGained(evt);
             }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                jTableEquiposInscriptosFocusLost(evt);
-            }
         });
         jScrollPane2.setViewportView(jTableEquiposInscriptos);
         if (jTableEquiposInscriptos.getColumnModel().getColumnCount() > 0) {
-            jTableEquiposInscriptos.getColumnModel().getColumn(3).setMinWidth(0);
-            jTableEquiposInscriptos.getColumnModel().getColumn(3).setPreferredWidth(0);
-            jTableEquiposInscriptos.getColumnModel().getColumn(3).setMaxWidth(0);
+            jTableEquiposInscriptos.getColumnModel().getColumn(0).setMinWidth(0);
+            jTableEquiposInscriptos.getColumnModel().getColumn(0).setPreferredWidth(0);
+            jTableEquiposInscriptos.getColumnModel().getColumn(0).setMaxWidth(0);
         }
+
+        jScrollPane4.setBorder(javax.swing.BorderFactory.createTitledBorder("Equipos sin Inscribir"));
 
         jTableEquiposDisponibles.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -247,9 +278,6 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 jTableEquiposDisponiblesFocusGained(evt);
             }
-            public void focusLost(java.awt.event.FocusEvent evt) {
-                jTableEquiposDisponiblesFocusLost(evt);
-            }
         });
         jScrollPane4.setViewportView(jTableEquiposDisponibles);
         if (jTableEquiposDisponibles.getColumnModel().getColumnCount() > 0) {
@@ -258,61 +286,36 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
             jTableEquiposDisponibles.getColumnModel().getColumn(0).setMaxWidth(0);
         }
 
-        jRadioButtonDni3.setText("Categoria");
-
-        jRadioButtonApellido3.setText("Club");
-        jRadioButtonApellido3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButtonApellido3ActionPerformed(evt);
-            }
-        });
-
-        jRadioButtonDni4.setText("Nombre");
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(3, 3, 3)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(3, 3, 3)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTextFieldBuscar)
-                            .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButtonAgregar)
-                            .addComponent(jButtonQuitar))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
-                        .addComponent(jRadioButtonDni4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jRadioButtonApellido3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jRadioButtonDni3)))
+                    .addComponent(jButtonAgregar)
+                    .addComponent(jButtonQuitar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(3, 3, 3))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButtonDni3)
-                    .addComponent(jRadioButtonApellido3)
-                    .addComponent(jRadioButtonDni4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(67, Short.MAX_VALUE)
                         .addComponent(jButtonAgregar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButtonQuitar)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButtonQuitar)
+                        .addGap(129, 129, 129))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
                 .addGap(3, 3, 3))
         );
 
@@ -328,7 +331,7 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(1, 1, 1)
                 .addComponent(jPanelBotones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(3, 3, 3))
         );
@@ -344,45 +347,47 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButtonGuardarActionPerformed
 
-    private void jRadioButtonApellido3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButtonApellido3ActionPerformed
+    private void formInternalFrameClosed(javax.swing.event.InternalFrameEvent evt) {//GEN-FIRST:event_formInternalFrameClosed
+        this.unJInternalFrame.setVisible(true);        // TODO add your handling code here:
+    }//GEN-LAST:event_formInternalFrameClosed
+
+    private void jTableEquiposDisponiblesFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposDisponiblesFocusGained
+        jButtonAgregar.setEnabled(true);
+        jButtonQuitar.setEnabled(false);
+        jTableEquiposInscriptos.clearSelection();
+    }//GEN-LAST:event_jTableEquiposDisponiblesFocusGained
+
+    private void jTableEquiposInscriptosFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposInscriptosFocusGained
+        jButtonAgregar.setEnabled(false);
+        jButtonQuitar.setEnabled(true);
+        jTableEquiposDisponibles.clearSelection();
+    }//GEN-LAST:event_jTableEquiposInscriptosFocusGained
+
+    private void jButtonQuitarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonQuitarActionPerformed
+        Equipo unEquipoSeleccionado = unaControladoraGlobal.getEquipoBD((Long) jTableEquiposInscriptos.getValueAt(jTableEquiposInscriptos.getSelectedRow(), 0));
+        unaControladoraGlobal.quitarEquipoInscripto(unTorneo, unEquipoSeleccionado);
+        limpiarTabla(modeloTablaEquipoInscripto, modeloTablaEquipoDisponible);
+        cargarTabla();
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButtonApellido3ActionPerformed
+    }//GEN-LAST:event_jButtonQuitarActionPerformed
 
     private void jButtonAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAgregarActionPerformed
+        System.out.println(jTableEquiposDisponibles.getSelectedRow());
+        System.out.println(jTableEquiposDisponibles.getValueAt(jTableEquiposDisponibles.getSelectedRow(), 0));
         Equipo unEquipoSeleccionado = unaControladoraGlobal.getEquipoBD((Long) jTableEquiposDisponibles.getValueAt(jTableEquiposDisponibles.getSelectedRow(), 0));
         unaControladoraGlobal.agregarEquipoInscripto(unTorneo, unEquipoSeleccionado);
         limpiarTabla(modeloTablaEquipoInscripto, modeloTablaEquipoDisponible);
         cargarTabla();
     }//GEN-LAST:event_jButtonAgregarActionPerformed
 
-    private void jTableEquiposDisponiblesFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposDisponiblesFocusLost
-        this.jTableEquiposDisponibles.clearSelection();
-        jButtonAgregar.setEnabled(false);
-
-    }//GEN-LAST:event_jTableEquiposDisponiblesFocusLost
-
-    private void jTableEquiposInscriptosFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposInscriptosFocusLost
-        this.jTableEquiposInscriptos.clearSelection();
-        jButtonQuitar.setEnabled(false);// TODO add your handling code here:
-    }//GEN-LAST:event_jTableEquiposInscriptosFocusLost
-
-    private void jTableEquiposDisponiblesFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposDisponiblesFocusGained
-        jButtonAgregar.setEnabled(true);
-        jButtonQuitar.setEnabled(false);// TODO add your handling code here:
-    }//GEN-LAST:event_jTableEquiposDisponiblesFocusGained
-
-    private void jTableEquiposInscriptosFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTableEquiposInscriptosFocusGained
-        jButtonAgregar.setEnabled(false);
-        jButtonQuitar.setEnabled(true);// TODO add your handling code here:
-    }//GEN-LAST:event_jTableEquiposInscriptosFocusGained
-
-    private void jButtonQuitarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonQuitarActionPerformed
+    private void jButtonAgregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonAgregarMouseClicked
+        System.out.println(jTableEquiposDisponibles.getSelectedRow());
+        System.out.println(jTableEquiposDisponibles.getValueAt(jTableEquiposDisponibles.getSelectedRow(), 0));
         Equipo unEquipoSeleccionado = unaControladoraGlobal.getEquipoBD((Long) jTableEquiposDisponibles.getValueAt(jTableEquiposDisponibles.getSelectedRow(), 0));
-        unaControladoraGlobal.quitarEquipoInscripto(unTorneo, unEquipoSeleccionado);
+        unaControladoraGlobal.agregarEquipoInscripto(unTorneo, unEquipoSeleccionado);
         limpiarTabla(modeloTablaEquipoInscripto, modeloTablaEquipoDisponible);
         cargarTabla();
-     // TODO add your handling code here:
-    }//GEN-LAST:event_jButtonQuitarActionPerformed
+    }//GEN-LAST:event_jButtonAgregarMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -394,13 +399,9 @@ public class IEquiposTorneos extends javax.swing.JInternalFrame {
     private javax.swing.JButton jButtonQuitar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanelBotones;
-    private javax.swing.JRadioButton jRadioButtonApellido3;
-    private javax.swing.JRadioButton jRadioButtonDni3;
-    private javax.swing.JRadioButton jRadioButtonDni4;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTable jTableEquiposDisponibles;
     private javax.swing.JTable jTableEquiposInscriptos;
-    private javax.swing.JTextField jTextFieldBuscar;
     // End of variables declaration//GEN-END:variables
 }
