09e3f5f23837a7b0ccf7c32002ff56e1d4aafdee
Leanwit <leanwitzke@gmail.com>
2014-10-09 20:58:40 -0300

    Tabla de posiciones ordenado por puntos

--- src/DataSources/TablaPosicionesDS.java
+++ src/DataSources/TablaPosicionesDS.java
@@ -8,6 +8,10 @@ package DataSources;
 import Interfaces.IGestionEquipo;
 import java.io.File;
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,10 +29,159 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-/**
- *
- * @author Leanwit
- */
+// <editor-fold defaultstate="collapsed" desc="equipoTablaPosiciones">
+class equipoTablaPosiciones {
+
+    private int cantidadPartidosJugados;
+    private int cantidadPartidosGanados;
+    private int cantidadGoles;
+    private int cantidadGolesContrario;
+    private int cantidadPartidosEmpatados;
+    private int cantidadPartidosPerdidos;
+    private int puntos;
+    private int diferenciaGol;
+    private String nombreEquipo;
+
+    public equipoTablaPosiciones(int cantPartJug, int cantPartGan, int cantGoles, int cantGolesContrario, int cantPartEmpat, int cantPartPer, String nombreEquipo) {
+        this.cantidadPartidosJugados = cantPartJug;
+        this.cantidadPartidosGanados = cantPartGan;
+        this.cantidadGoles = cantGoles;
+        this.cantidadGolesContrario = cantGolesContrario;
+        this.cantidadPartidosEmpatados = cantPartEmpat;
+        this.cantidadPartidosPerdidos = cantPartPer;
+        this.puntos = (cantidadPartidosGanados * 3) + cantidadPartidosEmpatados;
+        this.diferenciaGol = cantidadGoles - cantidadGolesContrario;
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    /**
+     * @return the cantidadPartidosJugados
+     */
+    public int getCantidadPartidosJugados() {
+        return cantidadPartidosJugados;
+    }
+
+    /**
+     * @param cantidadPartidosJugados the cantidadPartidosJugados to set
+     */
+    public void setCantidadPartidosJugados(int cantidadPartidosJugados) {
+        this.cantidadPartidosJugados = cantidadPartidosJugados;
+    }
+
+    /**
+     * @return the cantidadPartidosGanados
+     */
+    public int getCantidadPartidosGanados() {
+        return cantidadPartidosGanados;
+    }
+
+    /**
+     * @param cantidadPartidosGanados the cantidadPartidosGanados to set
+     */
+    public void setCantidadPartidosGanados(int cantidadPartidosGanados) {
+        this.cantidadPartidosGanados = cantidadPartidosGanados;
+    }
+
+    /**
+     * @return the cantidadGoles
+     */
+    public int getCantidadGoles() {
+        return cantidadGoles;
+    }
+
+    /**
+     * @param cantidadGoles the cantidadGoles to set
+     */
+    public void setCantidadGoles(int cantidadGoles) {
+        this.cantidadGoles = cantidadGoles;
+    }
+
+    /**
+     * @return the cantidadGolesContrario
+     */
+    public int getCantidadGolesContrario() {
+        return cantidadGolesContrario;
+    }
+
+    /**
+     * @param cantidadGolesContrario the cantidadGolesContrario to set
+     */
+    public void setCantidadGolesContrario(int cantidadGolesContrario) {
+        this.cantidadGolesContrario = cantidadGolesContrario;
+    }
+
+    /**
+     * @return the cantidadPartidosEmpatados
+     */
+    public int getCantidadPartidosEmpatados() {
+        return cantidadPartidosEmpatados;
+    }
+
+    /**
+     * @param cantidadPartidosEmpatados the cantidadPartidosEmpatados to set
+     */
+    public void setCantidadPartidosEmpatados(int cantidadPartidosEmpatados) {
+        this.cantidadPartidosEmpatados = cantidadPartidosEmpatados;
+    }
+
+    /**
+     * @return the cantidadPartidosPerdidos
+     */
+    public int getCantidadPartidosPerdidos() {
+        return cantidadPartidosPerdidos;
+    }
+
+    /**
+     * @param cantidadPartidosPerdidos the cantidadPartidosPerdidos to set
+     */
+    public void setCantidadPartidosPerdidos(int cantidadPartidosPerdidos) {
+        this.cantidadPartidosPerdidos = cantidadPartidosPerdidos;
+    }
+
+    /**
+     * @return the puntos
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+    /**
+     * @param puntos the puntos to set
+     */
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    /**
+     * @return the diferenciaGol
+     */
+    public int getDiferenciaGol() {
+        return diferenciaGol;
+    }
+
+    /**
+     * @param diferenciaGol the diferenciaGol to set
+     */
+    public void setDiferenciaGol(int diferenciaGol) {
+        this.diferenciaGol = diferenciaGol;
+    }
+
+    /**
+     * @return the nombreEquipo
+     */
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    /**
+     * @param nombreEquipo the nombreEquipo to set
+     */
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+}
+//</editor-fold> 
+
 public class TablaPosicionesDS implements JRDataSource {
 
     ControladoraGlobal unaControladoraGlobal;
@@ -37,25 +190,35 @@ public class TablaPosicionesDS implements JRDataSource {
     List<Equipo> equipos;
     int indiceEquipos = -1;
     private DateFormat df = DateFormat.getDateInstance();
-
-    int cantidadPartidosJugados = 0;
-    int cantidadPartidosGanados = 0;
-    int cantidadGoles = 0;
-    int cantidadGolesContrario = 0;
-    int cantidadPartidosEmpatados = 0;
-    int cantidadPartidosPerdidos = 0;
-    int puntos = 0;
+    private equipoTablaPosiciones unEquipoTabla;
+    private List<equipoTablaPosiciones> listaTablaPosiciones = new ArrayList();
 
     public TablaPosicionesDS(ControladoraGlobal unaControladoraGlobal, Torneo unTorneoSeleccionado) {
         this.unaControladoraGlobal = unaControladoraGlobal;
         this.unTorneo = unTorneoSeleccionado;
         this.fechasTorneo = (List<FechaTorneo>) unTorneo.getFechasTorneo();
         this.equipos = (List<Equipo>) unTorneo.getEquiposInscriptos();
+        for (Equipo unEquipo : equipos) {
+            calcularTablaPosiciones(unEquipo);
+        }
+        Collections.sort(listaTablaPosiciones, new Comparator<equipoTablaPosiciones>() {
+        @Override
+        public int compare(equipoTablaPosiciones  equipo1, equipoTablaPosiciones  equipo2)
+        {
+            
+            int valor = ((Integer)equipo2.getPuntos()).compareTo((Integer)equipo1.getPuntos());
+            if(valor == 0){
+                return  ((Integer)equipo2.getDiferenciaGol()).compareTo((Integer)equipo1.getDiferenciaGol());
+            }
+            return valor;
+        }
+    });
+        //Collections.sort(listaTablaPosiciones, (equipoTablaPosiciones equipo1, equipoTablaPosiciones equipo2) -> ((Integer) equipo2.getPuntos()).compareTo((Integer) equipo1.getPuntos()));
     }
 
     @Override
     public boolean next() throws JRException {
-        return ++indiceEquipos < equipos.size(); //To change body of generated methods, choose Tools | Templates.
+        return ++indiceEquipos < listaTablaPosiciones.size(); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
@@ -66,37 +229,35 @@ public class TablaPosicionesDS implements JRDataSource {
         } else if ("ruta".equals(jrf.getName())) {
             valor = unaControladoraGlobal.rutaSistema();
         } else if ("equipo".equals(jrf.getName())) {
-            cantidadPartidosJugados = 0;
-            cantidadPartidosGanados = 0;
-            cantidadGoles = 0;
-            cantidadGolesContrario = 0;
-            cantidadPartidosEmpatados = 0;
-            cantidadPartidosPerdidos = 0;
-            puntos = 0;
-            calcularTablaPosiciones(equipos.get(indiceEquipos));
-            valor = equipos.get(indiceEquipos).getNombre();
+            valor = listaTablaPosiciones.get(indiceEquipos).getNombreEquipo();
         } else if ("pj".equals(jrf.getName())) {
-            valor = cantidadPartidosJugados;
+            valor = listaTablaPosiciones.get(indiceEquipos).getCantidadPartidosJugados();
 
         } else if ("pg".equals(jrf.getName())) {
-            valor = cantidadPartidosGanados;
+            valor = listaTablaPosiciones.get(indiceEquipos).getCantidadPartidosGanados();
         } else if ("pe".equals(jrf.getName())) {
-            valor = cantidadPartidosEmpatados;
+          valor = listaTablaPosiciones.get(indiceEquipos).getCantidadPartidosEmpatados();
         } else if ("pp".equals(jrf.getName())) {
-            valor = cantidadPartidosPerdidos;
+           valor = listaTablaPosiciones.get(indiceEquipos).getCantidadPartidosPerdidos();
         } else if ("ga".equals(jrf.getName())) {
-            valor = cantidadGoles;
+            valor = listaTablaPosiciones.get(indiceEquipos).getCantidadGoles();
         } else if ("gc".equals(jrf.getName())) {
-            valor = cantidadGolesContrario;
+           valor = listaTablaPosiciones.get(indiceEquipos).getCantidadGolesContrario();
         } else if ("dg".equals(jrf.getName())) {
-            valor = cantidadGoles - cantidadGolesContrario;
+           valor = listaTablaPosiciones.get(indiceEquipos).getDiferenciaGol();
         } else if ("pts".equals(jrf.getName())) {
-            valor = (cantidadPartidosGanados * 3 + cantidadPartidosEmpatados);
+           valor = listaTablaPosiciones.get(indiceEquipos).getPuntos();
         }
         return valor;//To change body of generated methods, choose Tools | Templates.
     }
 
     private void calcularTablaPosiciones(Equipo unEquipo) {
+        int cantidadPartidosJugados = 0;
+        int cantidadPartidosGanados = 0;
+        int cantidadGoles = 0;
+        int cantidadGolesContrario = 0;
+        int cantidadPartidosEmpatados = 0;
+        int cantidadPartidosPerdidos = 0;        
         for (FechaTorneo unaFechaTorneo : fechasTorneo) {
             for (Partido unPartido : unaFechaTorneo.getPartidos()) {
                 if (unPartido.getNombreVeedor() != null) {
@@ -137,7 +298,9 @@ public class TablaPosicionesDS implements JRDataSource {
                 }
             }
         }
-       
+        equipoTablaPosiciones unEquipoTabla = new equipoTablaPosiciones(cantidadPartidosJugados, cantidadPartidosGanados, cantidadGoles, cantidadGolesContrario, cantidadPartidosEmpatados, cantidadPartidosPerdidos, unEquipo.getNombre());
+        listaTablaPosiciones.add(unEquipoTabla);
+
     }
 
     public void verReporte() {
