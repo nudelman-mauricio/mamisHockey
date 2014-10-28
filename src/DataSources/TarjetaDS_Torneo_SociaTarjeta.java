a02e2dfbc8dab06a5e5954119c581f3b2db6697e
Leandro <leanwitzke@gmail.com>
2014-10-28 12:26:12 -0300

    Cambios para evitar Null

--- src/DataSources/TarjetaDS_Torneo_SociaTarjeta.java
+++ src/DataSources/TarjetaDS_Torneo_SociaTarjeta.java
@@ -25,14 +25,14 @@ import net.sf.jasperreports.engine.JRField;
  */
 public class TarjetaDS_Torneo_SociaTarjeta implements JRDataSource {
 
-    ControladoraGlobal unaControladoraGlobal;    
-    Socia unaSocia;   
+    ControladoraGlobal unaControladoraGlobal;
+    Socia unaSocia;
     List<Tarjeta> listaTarjetas = new ArrayList();
     int indiceTarjetas = -1;
     private DateFormat df = DateFormat.getDateInstance();
 
     public TarjetaDS_Torneo_SociaTarjeta(List<Tarjeta> listaTarjetas, ControladoraGlobal unaControladoraGlobal, Socia unaSocia) {
-        
+
         this.unaControladoraGlobal = unaControladoraGlobal;
         this.unaSocia = unaSocia;
         this.listaTarjetas = listaTarjetas;
@@ -55,20 +55,31 @@ public class TarjetaDS_Torneo_SociaTarjeta implements JRDataSource {
         } else if ("tipoTarjeta".equals(jrf.getName())) {
             valor = listaTarjetas.get(indiceTarjetas).getTipo();
         } else if ("fechaTorneo".equals(jrf.getName())) {
-            valor = unaControladoraGlobal.getFechaTorneoDePartido(unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas))).getNumeroFecha();
+            if (unaControladoraGlobal.getFechaTorneoDePartido(unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas))) != null) {
+                valor = unaControladoraGlobal.getFechaTorneoDePartido(unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas))).getNumeroFecha();
+            } else {
+                valor = "-";
+            }
         } else if ("partido".equals(jrf.getName())) {
-            valor = unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas)).getUnEquipoLocal()+" vs "+ unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas)).getUnEquipoVisitante();
-        }else if ("minuto".equals(jrf.getName())) {
-            valor = listaTarjetas.get(indiceTarjetas).getMinuto() + "' " +listaTarjetas.get(indiceTarjetas).getTiempo()+"T";
-        }
-        else if ("contabilizo".equals(jrf.getName())) {
-            if(listaTarjetas.get(indiceTarjetas).isComputado()){
+            if (unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas)) != null) {
+                valor = unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas)).getUnEquipoLocal() + " vs " + unaControladoraGlobal.getPartidoTarjeta(listaTarjetas.get(indiceTarjetas)).getUnEquipoVisitante();
+            } else {
+                valor = "Acumulada";
+            }
+        } else if ("minuto".equals(jrf.getName())) {
+            if (listaTarjetas.get(indiceTarjetas).getMinuto() != null) {
+                valor = listaTarjetas.get(indiceTarjetas).getMinuto() + "' " + listaTarjetas.get(indiceTarjetas).getTiempo() + "T";
+            } else {
+                valor = "-";
+            }
+        } else if ("contabilizo".equals(jrf.getName())) {
+            if (listaTarjetas.get(indiceTarjetas).isComputado()) {
                 valor = "Si";
             } else {
                 valor = "No";
-            }            
+            }
         }
         return valor;
-    }  
+    }
 
 }
