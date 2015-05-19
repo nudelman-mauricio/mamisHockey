be367068c1e74cc4258d1f0ea39dbd68ba567ed0
Nudelman Mauricio <nudelman.mauricio@gmail.com>
2015-05-19 16:13:05 -0300

    error vencimientos

--- src/Interfaces/IGestionIngresosFuturos.java
+++ src/Interfaces/IGestionIngresosFuturos.java
@@ -142,8 +142,8 @@ public class IGestionIngresosFuturos extends javax.swing.JInternalFrame {
                                 montoRestantePorConcepto += montoASumar(unaCuota);
                                 montoRestantePorDeuda += montoASumar(unaCuota);
                                 if (unaControladoraGlobal.fechaSistema().after(unaCuota.getFechaVencimiento())) {
-                                    montoVencidoPorConcepto += montoRestantePorConcepto;
-                                    montoVencidoPorDeuda += montoRestantePorDeuda;
+                                    montoVencidoPorConcepto += montoASumar(unaCuota);
+                                    montoVencidoPorDeuda += montoASumar(unaCuota);
                                 }
                                 bandera = true;
                             } else {
@@ -151,8 +151,8 @@ public class IGestionIngresosFuturos extends javax.swing.JInternalFrame {
                                     if (unaControladoraGlobal.fechaSistema().after(unaCuota.getFechaVencimiento())) {
                                         montoRestantePorConcepto += montoASumar(unaCuota);
                                         montoRestantePorDeuda += montoASumar(unaCuota);
-                                        montoVencidoPorConcepto += montoRestantePorConcepto;
-                                        montoVencidoPorDeuda += montoRestantePorDeuda;
+                                        montoVencidoPorConcepto += montoASumar(unaCuota);
+                                        montoVencidoPorDeuda += montoASumar(unaCuota);
                                         bandera = true;
                                     }
                                 } else { //Si solo hay que mostrar los no vencidos
@@ -246,9 +246,9 @@ public class IGestionIngresosFuturos extends javax.swing.JInternalFrame {
         jRadioButtonTodos = new javax.swing.JRadioButton();
 
         setClosable(true);
-        setMaximumSize(new java.awt.Dimension(900, 700));
-        setMinimumSize(new java.awt.Dimension(900, 700));
-        setPreferredSize(new java.awt.Dimension(900, 700));
+        setMaximumSize(new java.awt.Dimension(900, 694));
+        setMinimumSize(new java.awt.Dimension(900, 694));
+        setPreferredSize(new java.awt.Dimension(900, 694));
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -545,7 +545,7 @@ public class IGestionIngresosFuturos extends javax.swing.JInternalFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTextFieldVencido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabelVencido))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
